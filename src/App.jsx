bb66a9305eef8cc8fa299f9faa6cfc5ea628cda8
Abhishek <abhishek29112005@gmail.com>
2026-06-17 15:50:07 +0530

    setup layout

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router'
+import { Header, Footer } from './components'
+import './App.css'
+function App() {
+  return(
+    <div className='min-h-screen flex flex-col items-center text-white'>
+      <Header/>
+      <main className='flex-1'>
+      {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
