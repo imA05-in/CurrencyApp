dd74d91f880d6a5f0255a1855441ee0a293e4b16
Abhishek <abhishek29112005@gmail.com>
2026-06-17 15:49:48 +0530

    currency rate hook

---
+++ src/hooks/useCurrencyHook.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export default function useCurrencyHook() {
+  const [currency, setCurrency] = useState("usd");
+  const [res, setRes] = useState("");
+  useEffect(() => {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+    .then((response) => {return response.json()})
+    .then((jsonRes)=>setRes(jsonRes))
+  }, []);
+  return res;
+}
